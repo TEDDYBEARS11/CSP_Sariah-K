2fcf6920ea579b393ac33fefda6175199a2e5a4d
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-01-30 11:50:01 -0700

    notes for c

--- Variables/practice.py
+++ Variables/practice.py
@@ -1,1 +1,13 @@
 # sariah kamore, variables notes
+name = "sariah"
+
+subject = input("what is your favorite subject:")
+print(name, "really likes", subject)
+
+subject = input("what is a number between 1 and 10: ")
+
+subject = input("what is your favotire color: ")
+
+subject = input("what is your age: ")
+
+subject = input("What is your favorite shape: " )
