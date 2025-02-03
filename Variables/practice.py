befb7b2b8066737a2c6da5cb7ce18c37f1fa8534
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-03 08:43:00 -0700

    first program

--- Variables/practice.py
+++ Variables/practice.py
@@ -3,14 +3,4 @@ name = "sariah"
 subject = "english"
 print(name, "really liked", subject)
 subject = input("what is your favorite color:")
-eye_color = "brown"
-subject = input("what is your eye color:")
-age = 15
-year = "2025"
-school = "UCAS"
-subject = input ("what school do you go to: ")
-number = 12
-print(name, "really liked", number)
-breakfast = "panckakes"
-big_number = 100
-favorite_color = "blue"
+output = name + "really liked" + subject
