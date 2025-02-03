347b57ef002ffaac40ab8441a847dd460af555cd
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-03 08:47:25 -0700

    homework

--- notes.c
+++ notes.c
@@ -4,23 +4,11 @@ char name[] = "sariah";
 int age = 15;
 float pi = 3.14;
 float e = 2.71;
-num = 5;
-year = 2025;
-school = "Utah county academy of science";
-grade = 9
-town = "Utah county";
-food = "anything";
 
 int main(void){
     printf("Hello I am %s. I am %d years old. I like the number %f.\n", name, age, pi);
     printf("%d\n", age);
     printf("%f\n", pi);
     printf("%f\n", e);
-    printf("%f\n", num);
-    printf("%f\n", year);
-    printf("%f\n", school);
-    printf("%f\n", grade);
-    printf("%f\n", town);
-    printf("%f\n", food);
     return 0;
 }
