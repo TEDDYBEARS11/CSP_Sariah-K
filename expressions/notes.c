6833eb30902ecb21be80abcb9f76b8c635cf09e7
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-04 11:17:09 -0700

    finishing notes

---
+++ expressions/notes.c
@@ -0,0 +1,13 @@
+// Sariah Kamore, expressions notes c
+#include <stdio.h>
+#include <math.h>
+//float x = pow(2,2);
+//float equation = 5*7+11/(5-x);
+float pi = 3.14159;
+// + - * /
+int main(void){
+    //printf("Hello world\n");
+//printf("%f\n", equation);
+printf("%f\n, pi");
+    return 0;
+}
--- notes.c
+++ notes.c
@@ -1,14 +1,21 @@
+// Sariah kamore, Varoables notes c
 #include <stdio.h>
 
-char name[] = "sariah";
+char name[20] = "sariah";
 int age = 15;
 float pi = 3.14;
 float e = 2.71;
 
 int main(void){
+    printf("welcome, what is your name; \n");
+    scanf("%s", name);
+    printf("how old are you?\n");
+    scanf("%d", &age);
+    printf("write out as much as pi as you know");
+    scanf("%f", &pi);
     printf("Hello I am %s. I am %d years old. I like the number %f.\n", name, age, pi);
-    printf("%d\n", age);
-    printf("%f\n", pi);
-    printf("%f\n", e);
+    //printf("%d\n", age);
+    //printf("%f\n", pi);
+    //printf("%f\n", e);
     return 0;
 }
