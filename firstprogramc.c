456e32623bc542670fb344c75eb93dc9fb7f0adb
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-05 08:57:10 -0700

    first program c

---
+++ firstprogramc.c
@@ -0,0 +1,13 @@
+// Sariah kamore, first program c
+#include <stdio.h>
+
+char name[20];
+
+
+int main(void){
+    printf("welcome, what is your name; \n");
+    scanf("%s\n", name);
+    printf("%s\n", name);
+    printf("hello, dear user");
+    return 0;
+}
