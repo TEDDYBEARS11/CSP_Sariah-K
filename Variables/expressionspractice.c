42843b5915ae7bf41de872678226afc0a7a68c42
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-05 11:48:30 -0700

    expressions practice c

---
+++ Variables/expressionspractice.c
@@ -0,0 +1,20 @@
+// Sariah Kamore, expressions c
+
+#include <stdio.h>
+#include <math.h>
+float equation = 7-24/8*4+6;
+float a = 18/3-7+2*5;
+float b = 6*4/12+72/8-9;
+float c= (17-6/2)+4*3;
+float d= -2*(1*4-4/2)+(6+2-3);
+float e = 
+// + - * /
+int main(void){
+    //printf("Hello world\n");
+printf("float equation = %f\n", equation);
+printf("float a = %f\n", a);
+printf("float b= %f\n", b);
+printf("float c= %f\n", c);
+printf("float d= %f\n", d);
+    return 0;
+}
--- Variables/firstproject.py
+++ Variables/firstproject.py
@@ -1,6 +1,6 @@
 # sariah kamore, first program python
 name = input ("put your name here: ")
 subject = "english"
-print(name, "really liked", subject)
+print(name, "really likes", subject)
 subject = input("what is your favorite color:")
 output = name + "really liked" + subject
