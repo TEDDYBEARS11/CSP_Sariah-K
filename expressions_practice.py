3633ce647797928afc6e257d4a83b52df239fc71
TEDDYBEARS11 <Sariah.Kamore@ucas-edu.net>
2025-02-07 04:31:42 +0000

    expressions practice python

---
+++ expressions_practice.py
@@ -0,0 +1,18 @@
+# sariah kamore, expressions practice python
+N1 = 7 
+N2 = 24
+N3 = 8
+N4 = 4
+N5 = 6
+solution1 = N1-N2/N3*N4+N5
+print(solution1)
+N6, N7, N8, N9 = 18, 3, 2, 5
+solution2 = N6/N7-N1+N8*N9
+print(solution2)
+N10, N11, N12, N13 = 6, 12, 72, 9
+solution3= N10*N4/N11+N12/N3-N13
+print(solution3)
+N14 = 17 
+solution4 = (N14-N5/N8)+N4*N7
+print(solution4)
+N15, N16 = -1, -2
