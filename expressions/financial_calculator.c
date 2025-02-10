dddb1e50741587907623192bac923cae2956302c
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-10 11:49:13 -0700

    notes and financial calculator

--- expressions/financial_calculator.c
+++ expressions/financial_calculator.c
@@ -6,3 +6,38 @@ int main(void)
     printf("Hello world");
     return 0;
 }
+//ask what their income is (variable an input)
+
+//ask what their rent is (variable that is an input)
+
+//ask what their untilites is (variable that is an input)
+
+//ask what their groceries is (variable that is an input)
+
+//ask what their transportation is (variable that is an input)
+
+//calculate savings as 10% of income (income*.1)
+
+//calculate spending as income-rent-utilities-groceries-transportaion-savings (variable)
+
+//calculate percent income of rent(rent/income*100) (variable)
+
+//calculate percent income of utilities(utilities/income*100) (variable)
+
+//calculate percent income of groceries(groceries/income*100) (variable)
+
+//calculate percent income of transportation(transportation/income*100) (variable)
+
+//calculate percent income of spending(spending/income*100) (variable)
+
+//Your rent is $xx.xx which is xx% of your income. (print)
+
+//Your utilities is $xx.xx which is xx% of your income. (print)
+
+//Your groceries is $xx.xx which is xx% of your income. (print)
+
+//Your tansportation is $xx.xx which is xx% of your income. (print)
+
+//Your savings is $xx.xx which is xx% of your income. (print)
+
+//Your spending is $xx.xx which is xx% of your income. (print)
