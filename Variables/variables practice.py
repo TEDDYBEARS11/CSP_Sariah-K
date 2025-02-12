6b71e3ae61a9e5ecf922e9b8471f42e781306b48
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-12 11:49:45 -0700

    variables practice python

---
+++ Variables/variables practice.py
@@ -0,0 +1,6 @@
+# sariah kamore, variables practice python
+name = input("put your name here: ")
+subject = input("what is your favorite class:")
+number = input("what is your favorite number between 1 and 10: ")
+big_number =  input("what i your fvorite number between 100 and 1000:")
+color = input("what is your favorite color:")
