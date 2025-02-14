d57d6080bce2f37cf40357e0345b218890c82cd3
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-14 11:36:03 -0700

    silly sentences python

---
+++ strings/silly_sentence.py
@@ -0,0 +1,7 @@
+#sariah Kamore, silly sentence python
+
+noun = input("Enter a noun: ")
+verb= input("Enter a verb: ") 
+adjective = input("Enter an adjective: ")
+adverb = input("Enter an adverb: ")
+print("The " + adjective + " " + noun + " " + verb + " " + adverb + "!")
