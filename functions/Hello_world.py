4c89a060436f02c10e2985af605dde86a4f873b5
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-19 11:30:03 -0700

    hello world python

---
+++ functions/Hello_world.py
@@ -0,0 +1,15 @@
+#sariah Kamore hello world python
+
+def name ():
+    print("what is your name?")
+    name = input()
+    print("hello", name)
+#name()
+
+
+name()
+name()
+name()
+name()
+name()
+name()
