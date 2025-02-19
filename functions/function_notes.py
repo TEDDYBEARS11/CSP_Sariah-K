ab27284f29c877ed2356ef34f2800e48351be663
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-19 11:06:03 -0700

    template notes

---
+++ functions/function_notes.py
@@ -0,0 +1,18 @@
+# sariah kamore, functions notes python
+
+#functions= places that hold actions
+
+#def add():
+  #  num1= int(input("please tell me a number:\n"))
+   #print(num1+num2)
+    #print("the sum of the numbers is", num1+num2)
+
+#add()
+
+def add(num1, num2):
+    print(num1+num2)
+
+add(5, 6)
+add(10, 20)
+add(100, 200)
+add(1000, 2000)
