2f24ebf1aa2af3e447233c94020b40fbb099b97f
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-19 11:48:54 -0700

    hello world c

---
+++ functions/hello_world.c
@@ -0,0 +1,17 @@
+//sariah kamore hello world c
+#include <stdio.h>
+
+//function is written that says hello to given name
+//function is called with 5 different names
+
+void(char name[])
+
+{
+    printf("what is your name?\n");
+    scanf("%s", name);
+    printf("Hello %s\n", name);
+    return 0;
+}
+
+
+
