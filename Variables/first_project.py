3021c3bd3c61daecdd6e35a773f8005748e7e778
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-19 11:17:06 -0700

    first project python

---
+++ Variables/first_project.py
@@ -0,0 +1,6 @@
+#sariah Kamore first project python
+
+name = input("What is your name?\n")
+subject = input("What is your favorite subject?\n")
+color = input("what is yout favorite color?\n")
+print("My name is", name, "and my favorite subject is", subject, "and my favorite color is", color)
