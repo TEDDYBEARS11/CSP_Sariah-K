292d4cf043ce95d5aca67ddd4457585760926ff5
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-20 11:25:17 -0700

    financial calculator python

---
+++ functions/financial_calc_update.py
@@ -0,0 +1,28 @@
+#sariah kamore financial calculator python
+def info(cost, income, type):
+    percent = cost/income *100
+    print(f"your {type} is ${cost:.2f} which is {percent}% of your income.")
+
+
+
+print("welcome to my financial calculator that showa your finances.")
+#ask what their income is (variable an input)
+income = float(input("what is your income?\n"))
+#ask what their rent is (variable that is an input)
+rent = float(input("what is your rent?\n"))
+#ask what their untilites is (variable that is an input)
+utilities = float(input("what are your utilities?\n"))
+#ask what their groceries is (variable that is an input)
+groceries = float(input("what is your groceries?\n"))
+#ask what their transportation is (variable that is an input)
+transportation = float(input("what is your transportation\n"))
+#calculate savings as 10% of income (income*.1)
+savings = income *.1
+spending = income-rent-utilities-groceries-transportation-savings
+
+info(rent, income, "rent")
+info(utilities, income, "utilities")
+info(groceries, income, "groceries")
+info(transportation, income, "transpotration")
+info(savings, income, "savings")
+info(spending, income, "spending")
