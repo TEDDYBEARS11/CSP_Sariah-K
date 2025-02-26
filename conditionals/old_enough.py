eaa509a57a7a03cdf04465218ac2e02cda733684
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-02-26 11:49:11 -0700

    old enough python

--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -1,1 +1,14 @@
 # sariah kamore old enough python
+
+age = int(input("how old are you: "))
+
+if age ==5:
+    print("you can go to school")
+elif age == 15:
+    print("you can get a learners permit")
+elif age == 18:
+    print("you can vote")
+elif age == 16:
+    print("you can drive")
+elif age <= 35:
+        print("you are old")
