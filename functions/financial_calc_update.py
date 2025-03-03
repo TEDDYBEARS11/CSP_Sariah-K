6a0a408c0c31c3241c82a075a621a07983377ee7
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-03-03 11:48:34 -0700

    financial calc updated and notes

--- functions/financial_calc_update.py
+++ functions/financial_calc_update.py
@@ -10,13 +10,13 @@ utilities = user("utilites")
 groceries = user("groceries")
 transportation  = user("transportation")
 savings = income*0.1
-expenses = rent+utilites+groceries+transportation+savings
+expenses = rent+utilities+groceries+transportation+savings
 
 def info(income, amount, type):
     percent_type = float(amount/income*100)
     print(f"you spend ${amount:.2f} in {type} and that is {percent_type:.1f}% of your income")
 info(income, rent, "rent")
-info(income, utilites, "utilities")
+info(income, utilities, "utilities")
 info(income, groceries, "groceries")
 info(income, transportation, "transportation")
 print(f"you have ${savings:.2f} and that is 10% of your income")
---
+++ loops/notes.py
@@ -0,0 +1,12 @@
+#sariah kamore notes loops python
+
+#2 types of loops
+nums = [12, 3,4, 5]
+for num in nums:
+    print(num)
+
+    #while loop
+    x=0
+while x < 10:
+    print(x)
+    x += 1
