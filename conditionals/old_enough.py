6c6ccb666a5db9eb46c01a12f2019fcccdd121cd
TEDDYBEARS11 <Sariah.Kamore@ucas-edu.net>
2025-03-03 04:22:11 +0000

    old enough python

--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -1,18 +1,15 @@
 # sariah kamore old enough python
 
-num = int(input("how old are you: "))
-
-if num == 0:
-    print("you are a baby")
-elif num <=18:
-    print("you can vote")
-elif num <=5:
-    print("you can go to school")
-elif num <=15:
-    print("you can get a permit")
-elif num <=16:
-    print("you can drive")
-elif num <35:
+age = int(input("how old are you?\n"))
+if age >=18:
+    print("you can to vote, get a permit, drive, and go to school")
+elif age >=16:
+    print("you can have a permit, drive, and go to school. You cannot vote.")
+elif age>=15:
+    print("you can can only get a learners permit and go to school.")
+elif age >= 4:
+    print("You are only able to go to school.")
+elif age <= 35:
     print("you are old")
 else:
-    print("you are ancient")
+    print("you are not able to to do any of the following. Vote, school, drive, or permit.")
