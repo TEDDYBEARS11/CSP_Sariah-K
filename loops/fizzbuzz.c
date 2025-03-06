40821894c3e8a44dfcf55005a3fc1da789bb9fb7
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-03-06 11:39:19 -0700

    fizz buzz c

---
+++ loops/fizzbuzz.c
@@ -0,0 +1,21 @@
+//sariah kamore fizz buzz c
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void){
+    for(int i = 1; i <= 50; i++){
+        if(i % 3 == 0 && i % 5 == 0){
+            printf("FizzBuzz\n");
+        } else if(i % 3 == 0){
+            printf("Fizz\n");
+        } else if(i % 5 == 0){
+            printf("Buzz\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }
+  
+
+    return 0;
+}
