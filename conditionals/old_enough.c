b956ca170bd18c5e51f397e5d304640d521208d4
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-03-06 11:06:08 -0700

    old enough c

--- conditionals/old_enough.c
+++ conditionals/old_enough.c
@@ -8,15 +8,15 @@ int main(void){
     scanf("%d", &age);
     if(age<5){ 
         printf("you are not able to go to school");
-    }else if (age == 5){
+    }else if (age <= 5){
         printf("you can only go to school");
     }else if(age <= 14){
         printf("you can go to school");
-    }else if (age == 15){
+    }else if (age <= 15){
         printf("you can get a permit and go to school.");
-    }else if (age == 16){
+    }else if (age <= 16){
         printf("you can drive, get a permit, and go to school.");
-    }else if (age == 17){
+    }else if (age <= 17){
         printf("you are able to drive, have a permit, and go to school but you cannot vote");
     }else{
         printf("you can vote, drive, go to school, and have a permit");
