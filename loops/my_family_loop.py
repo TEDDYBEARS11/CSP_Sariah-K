31e2ed4ccb12e74c1e8a8fcb92cfa1087145bddd
TEDDYBEARS11 <sariah.kamore@ucas-edu.net>
2025-03-06 11:21:31 -0700

    family loop python

---
+++ loops/my_family_loop.py
@@ -0,0 +1,8 @@
+#sariah Kamore my family loop python
+
+# This program will print out the names of my family members
+
+print("hello you are welcome to my family loop")
+people = ["archie", "liberty", "hilary", "isabella", "mysiah"]
+for person in people:
+    print(person, "is my family member")
