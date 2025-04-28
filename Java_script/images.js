890cc1f76aa7f00a81091707e202dc56ff809c65
sariah.kamore@ucas-edu.net <sariah.kamore@ucas-edu.net>
2025-04-28 11:16:59 -0600

    hidden image

--- Java_script/images.js
+++ Java_script/images.js
@@ -1,3 +1,9 @@
-function change(){
-  document.getElementById("hidden").style.display = "block"
+function view(){
+  if (document.getElementById("img").style.display = "block"){
+  document.getElementById("img").style.display = "none"
+  document.getElementById("shw").innerHTML = "show more"
+}else{
+  document.getElementById("more").style.display = "block"
+  document.getElementById("shw").innerHTML = "show less"
+}
 }
