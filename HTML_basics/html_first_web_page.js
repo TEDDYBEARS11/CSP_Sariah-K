6321499f22ed14e4d73e2a326bf0bf75861d8830
sariah.kamore@ucas-edu.net <sariah.kamore@ucas-edu.net>
2025-04-28 16:08:53 -0600

    lemons first web page

--- HTML_basics/html_first_web_page.js
+++ HTML_basics/html_first_web_page.js
@@ -1,11 +1,20 @@
-let images= "https://www.wallpapersshare.com/img/big/pink-lemon-slice-photo.jpg, https://uscitrusnursery.com/cdn/shop/articles/pink-variegated-lemons_551987ee-b504-400e-8673-25dce517f415_900x.jpg?v=1623611562"
+let images = ["https://as2.ftcdn.net/jpg/05/17/08/37/1000_F_517083703_jaDmUWZeVqCBc3ybAaTOleKHCpMMfukP.jpg, https://miamifruit.org/cdn/shop/products/image_dd18d821-864c-4812-9bf3-0995f9435717_300x.jpg?v=1706904533"]
 let counter = 0
-function change (){
+
+function change(){
     if(counter < images.length){
-        document.getElementById("stuff").src = images[counter]
-        counter += 1
-}else{
-    counter = 0
-    document.getElementById("stuff").src = images[counter]
+        document.getElementById("img"). src = images[counter]
+        [counter]
+        counter + 1
+    }else{ 
+        counter = 0
+        document.getElementById
+        ("img").src = images
+        [counter]
+    }
+    
 }
+
+function pop(){
+    window. alert("Actually, lemons that are pink aren't bright bright pink but are instead a lightish pink that seems to mix with orange! Pink lemons were first discovered on an ordinary Eureka lemons tree in Burbank, California, in 1931. They were the result of a spontaneous mutation and are now grown commercially in Southern California. Also known as the “variegated pink lemon” or the “variegated Eureka lemon” due to its splotchy, colorful foliage!")
 }
