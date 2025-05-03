f81cc3fea7ad04d74695c41e277ae416f35f6c16
TEDDYBEARS11 <Sariah.Kamore@ucas-edu.net>
2025-05-03 23:13:35 +0000

    final staycation

---
+++ HTML_basics/final_staycation_web_page.js
@@ -0,0 +1,4 @@
+function toggleContent() {
+    const content = document.getElementById("extraContent");
+    content.style.display = (content.style.display === "none" || content.style.display === "") ? "block" : "none";
+}
