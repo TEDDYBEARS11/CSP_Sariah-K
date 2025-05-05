eb73e7ab76c1d18d81363a21c42183e841f8e4bc
sariah.kamore@ucas-edu.net <sariah.kamore@ucas-edu.net>
2025-05-05 16:04:04 -0600

    final staycation

--- HTML_basics/final_staycation_web_page.js
+++ HTML_basics/final_staycation_web_page.js
@@ -3,6 +3,7 @@ function toggleContent() {
     content.style.display = (content.style.display === "none" || content.style.display === "") ? "block" : "none";
 }
 
-    function change(){ 
-        document.getElementById ("bike"). src = "https://allseasonsadventures.com/wp-content/uploads/2021/09/NOIMG_6645.jpg"
-    }
+
+function back(){
+    document.getElementById("bike").src = "https://allseasonsadventures.com/wp-content/uploads/2021/09/NOIMG_6645.jpg"
+}
